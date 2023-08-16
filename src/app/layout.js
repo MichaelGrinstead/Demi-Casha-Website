08303946a8b08a3845a3f2f6edde7d849b28342f
Michael Grinstead <mianclgr@gmail.com>
2023-08-16 21:17:59 +1000

    Remove Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import Header from '../components/Header'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -13,13 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        
-        <div className='page-section first'>
-        <Header />
-        {children}
+        <div>
+          {children}
 
         </div>
-        
+
         </body>
     </html>
   )
