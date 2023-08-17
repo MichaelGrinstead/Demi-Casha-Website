e9e6236fe1f53be6a6b763aee220ee6a7c87193f
Michael Grinstead <mianclgr@gmail.com>
2023-08-17 20:06:38 +1000

    Add paragaph elements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,30 @@
+
+
 export default function About() {
 
     return(
         <div className="page-section second" id="About">
             <div className="h-full flex flex-col justify-center items-center">
              <h1 className="text-brown text-5xl mt-12">About</h1>
+             <p className="text-brown text-2xl w-3/4 mt-12">
+             An electrifying psychedelic folk singer/songwriter with a resonance that far exceeds her years, Demi Casha’s particular brand of gum tree rock is quickly establishing her as one of Australia’s most exciting, emerging artists.
+ 
+
+             </p>
+             <p className="text-brown text-2xl w-3/4 mt-12">
+             December 2021 welcomed the release of Demi’s debut EP Return. Featuring a thumping 70s inspired sound that swings from Wild Woman, a heart wrenching salute to femininity to the upbeat and highly danceable Grow, Demi has earned regular rotation on ABC Sunshine Coast, Triple J Unearthed and 4ZZZ.
+             </p>
+             <p className="text-brown text-2xl w-3/4 mt-12">
+             With an impressive resume that includes opening for Karl S. Williams’ sell-out Brisbane show, the Sunshine Coasts dweller deliver refreshingly wild vocals sweeps over audiences like the ocean water that runs through her veins.
+             </p>
+             <p className="text-brown text-2xl w-3/4 mt-12">
+             The sheer verve and honesty of Demi Cashas performances have made her a regular fixture at blues and folk venues and festival stages including Caloundra Music Festival, The Bearded Lady, It’s Still A Secret and many more.
+
+             </p>
+             <p className="text-brown text-2xl w-3/4 mt-12 ">
+             Whether solo or performing with her band, you can hear a pin drop when Demi Casha takes the stage, but what follows is an emotional grenade that you’ll never forget.
+
+             </p>
 
             </div>
 
