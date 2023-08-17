b427148e47396327c9481db00adb514c609e46ca
Michael Grinstead <mianclgr@gmail.com>
2023-08-17 20:07:15 +1000

    Add home link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-
-
 export default function Navbar() {
     return (
         <nav className="flex flex-row gap-10 text-brown">
-                <a href="#Tour">Tour</a>
+                <a href="#Home">Home</a>
                 <a href="#About">About</a>
+                <a href="#Tour">Tour</a>
                 <a href="#Video">Video</a>
         </nav>
     )
