b6a9af68df3a29445c706671771e6abaf7d1db7a
Michael Grinstead <mianclgr@gmail.com>
2023-08-17 20:07:32 +1000

    Create sticky Navbar

---
+++ src/components/StickyNavbar.jsx
@@ -0,0 +1,10 @@
+export default function StickyNavbar() {
+    return (
+        <nav className="sticky top-0 flex flex-row justify-center items-center gap-10 text-brown">
+                <a href="#Home">Home</a>
+                <a href="#About">About</a>
+                <a href="#Tour">Tour</a>
+                <a href="#Video">Video</a>
+        </nav>
+    )
+}
