1e3680a4cdf6b6091f531eaaafbe484ed10a7d53
Michael Grinstead <mianclgr@gmail.com>
2023-08-18 20:12:10 +1000

    Format with prettier

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,18 +1,34 @@
 export default function Tour() {
-
-    return(
-        <div className="page-section third" id="Tour">
-            
-            <div className="h-full flex flex-col justify-center items-center">
-
-                <div className="mt-24 w-full">
-
-            <a href="https://www.songkick.com/artists/10287548" className="songkick-widget" data-theme="dark" data-track-button="on" data-detect-style="off" data-background-color="rgb(101,55,14,0.97)" data-font-color="rgb(255,255,255,1)" data-locale="en" data-other-artists="on" data-share-button="on" data-country-filter="on" data-rsvp="on" data-request-show="on" data-past-events="off" data-past-events-offtour="off" data-remind-me="off">The River Tour Dates</a>
-            <script defer src='//widget-app.songkick.com/injector/10287548'></script>
-                </div>
-
-            </div>
-
+  return (
+    <div className="page-section third" id="Tour">
+      <div className="h-full flex flex-col justify-center items-center">
+        <div className="mt-24 w-full">
+          <a
+            href="https://www.songkick.com/artists/10287548"
+            className="songkick-widget"
+            data-theme="dark"
+            data-track-button="on"
+            data-detect-style="off"
+            data-background-color="rgb(101,55,14,0.97)"
+            data-font-color="rgb(255,255,255,1)"
+            data-locale="en"
+            data-other-artists="on"
+            data-share-button="on"
+            data-country-filter="on"
+            data-rsvp="on"
+            data-request-show="on"
+            data-past-events="off"
+            data-past-events-offtour="off"
+            data-remind-me="off"
+          >
+            The River Tour Dates
+          </a>
+          <script
+            defer
+            src="//widget-app.songkick.com/injector/10287548"
+          ></script>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,18 +1,33 @@
 export default function Video() {
-    return(
-        <div className="page-section fourth" id="Video">
-            <div className="h-full flex flex-col justify-center items-center">
-            <div className="mt-24">
-                <iframe width="1120" height="630" src="https://www.youtube.com/embed/V84wgdr3_eo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-
-            </div>
-            <div className="mt-24
-            ">
-            <iframe width="1120" height="630" src="https://www.youtube.com/embed/SB1fRZU86uE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-
-            </div>
-
+  return (
+    <div className="page-section fourth" id="Video">
+      <div className="h-full flex flex-col justify-center items-center">
+        <div className="mt-24">
+          <iframe
+            width="1120"
+            height="630"
+            src="https://www.youtube.com/embed/V84wgdr3_eo"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
         </div>
-    )
+        <div
+          className="mt-24
+            "
+        >
+          <iframe
+            width="1120"
+            height="630"
+            src="https://www.youtube.com/embed/SB1fRZU86uE"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
 }
