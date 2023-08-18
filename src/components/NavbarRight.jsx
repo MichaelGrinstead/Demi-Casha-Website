035b00e82d5649d8989615bf361985bd5167d32e
Michael Grinstead <mianclgr@gmail.com>
2023-08-19 09:33:03 +1000

    Create right side navbar

---
+++ src/components/NavbarRight.jsx
@@ -0,0 +1,9 @@
+export default function NavbarRight() {
+  return (
+    <nav className="flex flex-row gap-10 text-brown font-semibold">
+      <a href="https://demicasha.bandcamp.com/">SHOP</a>
+      <a href="#Listen">LISTEN</a>
+      <a href="#Contact">CONTACT</a>
+    </nav>
+  );
+}
