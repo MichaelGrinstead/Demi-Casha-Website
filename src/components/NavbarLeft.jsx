16d1fd48ac7099ea9970daec4fc468f6c87de78d
Michael Grinstead <mianclgr@gmail.com>
2023-08-19 12:15:58 +1000

    Style

--- src/components/NavbarLeft.jsx
+++ src/components/NavbarLeft.jsx
@@ -1,8 +1,9 @@
 export default function NavbarLeft() {
   return (
     <nav className="flex flex-row gap-10 text-brown font-semibold">
-      <a href="#About">ABOUT</a>
       <a href="#Tour">TOUR</a>
+      <a href="#About">ABOUT</a>
+
       <a href="#Watch">WATCH</a>
     </nav>
   );
