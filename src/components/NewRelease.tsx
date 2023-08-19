bfa74512c80f3b97d761adbe1135820026de88dd
Michael Grinstead <mianclgr@gmail.com>
2023-08-19 12:16:13 +1000

    Create new release component

---
+++ src/components/NewRelease.tsx
@@ -0,0 +1,15 @@
+export default function NewRelease() {
+  return (
+    <div className="flex flex-col justify-center items-center w-1/2 mt-48 ml-8 font-semibold font text-white tracking-widest">
+      <h1 className=" text-7xl text-border-header">THE</h1>
+      <h1 className=" text-7xl text-border-header">RIVER</h1>
+      <h2 className="text-3xl mt-8 text-border">NEW SINGLE - OUT NOW!</h2>
+
+      <a href="https://gyro.to/147205TheRiver">
+        <button className="tracking-widest h-12 w-42 px-6 py-2 mt-8 bg-brown text-white rounded-full border border-white hover:bg-white hover:text-brown hover:border-brown">
+          LISTEN HERE
+        </button>
+      </a>
+    </div>
+  );
+}
