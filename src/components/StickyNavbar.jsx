ee7606c47b90fcd798e4c1e269ba7a9cb06deb40
Michael Grinstead <mianclgr@gmail.com>
2023-08-19 12:17:03 +1000

    Fix hydration bug

--- src/components/StickyNavbar.jsx
+++ src/components/StickyNavbar.jsx
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 export default function StickyNavbar() {
   const [showNavbar, setShowNavbar] = useState(false);
 
+  const navbarClass = showNavbar
+    ? "header-animation fixed top-0 flex flex-row justify-center items-center gap-6 text-white w-full h-10 bg-brown bg-opacity-80"
+    : "hidden";
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY >= window.innerHeight) {
@@ -19,17 +23,15 @@ export default function StickyNavbar() {
     };
   }, []);
 
-  if (showNavbar) {
-    return (
-      <nav className="header-animation fixed top-0 flex flex-row justify-center items-center gap-6 text-white w-full h-10 bg-brown bg-opacity-80">
-        <a href="#Home">HOME</a>
-        <a href="#About">ABOUT</a>
-        <a href="#Tour">TOUR</a>
-        <a href="#Watch">WATCH</a>
-        <a href="https://demicasha.bandcamp.com/">SHOP</a>
-        <a href="#Listen">LISTEN</a>
-        <a href="#Contact">CONTACT</a>
-      </nav>
-    );
-  }
+  return (
+    <nav className={navbarClass}>
+      <a href="#Home">HOME</a>
+      <a href="#Tour">TOUR</a>
+      <a href="#About">ABOUT</a>
+      <a href="#Watch">WATCH</a>
+      <a href="https://demicasha.bandcamp.com/">SHOP</a>
+      <a href="#Listen">LISTEN</a>
+      <a href="#Contact">CONTACT</a>
+    </nav>
+  );
 }
