903d8d1c92cf14114993878ce1f860b5f5a9a033
Michael Grinstead <mianclgr@gmail.com>
2023-08-19 17:03:03 +1000

    Add screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,16 @@ module.exports = {
         "white": "#faebd7"
       },
 
+      screens: {
+        'xxs': '320px',
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1200px',
+        '2xl': '1536px',
+      },
+
   
 
 
