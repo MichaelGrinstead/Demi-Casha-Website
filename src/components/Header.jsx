9759da41881e2e0c1ad05ab7f2c5a56a0d58783d
Michael Grinstead <mianclgr@gmail.com>
2023-08-19 12:15:14 +1000

    Remove socials

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,6 @@ import Image from "next/image";
 import logo from "../images/logo.png";
 import NavbarLeft from "./NavbarLeft";
 import NavbarRight from "./NavbarRight";
-import { SlSocialSpotify } from "react-icons/sl";
-import { BsInstagram } from "react-icons/bs";
-import { SlSocialYoutube } from "react-icons/sl";
 
 export default function Header() {
   return (
@@ -19,17 +16,6 @@ export default function Header() {
           height={150}
         />
         <NavbarRight />
-      </div>
-      <div className="flex flex-row absolute top-14 right-8 gap-2 justify-center items-center">
-        <a href="https://www.instagram.com/demicasha">
-          <BsInstagram className=" top-10 right-0 text-4xl text-brown h-7 w-7" />
-        </a>
-        <a href="https://open.spotify.com/artist/0xf5TLcShphohGq8Oikadq">
-          <SlSocialSpotify className=" top-10 right-0 text-4xl text-brown h-7 w-7" />
-        </a>
-        <a href="https://www.youtube.com/channel/UCoT9E8U3yuGYyerXWrs-AbA">
-          <SlSocialYoutube className=" top-10 right-0 text-4xl text-brown  h-9 w-9" />
-        </a>
       </div>
     </nav>
   );
