3854a09b8986c1212fb0577a072b31c597a275e1
Michael Grinstead <mianclgr@gmail.com>
2023-08-19 12:14:29 +1000

    Add Listen and Contact components

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,20 @@ import Landing from "../components/Landing"
 import About from "../components/About"
 import Watch from "../components/Watch"
 import Tour from "../components/Tour"
+import Listen from "../components/Listen"
+import Contact from "../components/Contact"
 
 
 export default function Home() {
   return (
     <main>
       <Landing />
+      <Tour />
       <About />
       <Watch />
-      <Tour />
+      <Listen/>
+      <Contact/>
+     
     </main>
   )
 }
