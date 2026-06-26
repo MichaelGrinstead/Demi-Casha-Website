817c40c7fe8bb3f33c003336e3baac5a51923540
Michael Grinstead <michael.a.grinstead@gmail.com>
2026-06-26 18:43:39 +1000

    chore: update youtube links

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -1,14 +1,11 @@
 export default function Watch() {
   return (
-    <div className="page-section fourth" id="Videos">
+    <div className="page-section fourth" id="Watch">
       <div className="h-full flex flex-col justify-center items-center">
-        <h2 className="text-brown text-2xl sm:text-3xl font-semibold text-center mt-24 mb-12">
-          VIDEOS
-        </h2>
-        <div className="aspect-w-16 aspect-h-9 max-w-full lg:w-[1120px] lg:h-[630px] xxs:w-[368px] xxs:h-[207px]">
+        <div className="aspect-w-16 aspect-h-9 mt-24 max-w-full lg:w-[1120px] lg:h-[630px] xxs:w-[368px] xxs:h-[207px]">
           <iframe
             className="w-full h-full"
-            src="https://www.youtube.com/embed/KyP1IucP_k4"
+            src="https://www.youtube.com/watch?v=izT_gWPN0Hs"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -18,7 +15,17 @@ export default function Watch() {
         <div className="aspect-w-16 aspect-h-9 mt-24 max-w-full lg:w-[1120px] lg:h-[630px] xxs:w-[368px] xxs:h-[207px]">
           <iframe
             className="w-full h-full"
-            src="https://www.youtube.com/embed/m43JdB1Fp2I"
+            src="https://www.youtube.com/watch?v=NUr0VWZKO2M"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="aspect-w-16 aspect-h-9 mt-24 max-w-full lg:w-[1120px] lg:h-[630px] xxs:w-[368px] xxs:h-[207px]">
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/watch?v=hM04EeJQGoI"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
